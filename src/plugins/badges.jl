fde0c811b886bbacba0307fdf2a8d882a54dff6e
Nick Robinson <npr251@gmail.com>
2020-08-26 18:51:23 +0100

    Update src/plugins/badges.jl
    Co-authored-by: Chris de Graaf <me@cdg.dev>

--- src/plugins/badges.jl
+++ src/plugins/badges.jl
@@ -14,7 +14,7 @@ function badges(::BlueStyleBadge)
     return Badge(
         "Code Style: Blue",
         "https://img.shields.io/badge/code%20style-blue-4495d1.svg",
-        "https://github.com/invenia/BlueStyle"
+        "https://github.com/invenia/BlueStyle",
     )
 end
 
