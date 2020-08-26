3804429e793c3d153846215484c7aba55c711f2e
Nick Robinson <nicholas.robinson@invenialabs.co.uk>
2020-08-26 17:51:46 +0100

    fixup! Add BlueStyleBadge and ColPracBadge Plugins

--- src/plugins/badges.jl
+++ src/plugins/badges.jl
@@ -28,7 +28,7 @@ struct ColPracBadge <: BadgePlugin end
 function badges(::ColPracBadge)
     return Badge(
         "ColPrac: Contributor's Guide on Collaborative Practices for Community Packages",
-        "https://img.shields.io/badge/ColPrac-Contributor's%20Guide-blueviolet)",
+        "https://img.shields.io/badge/ColPrac-Contributor's%20Guide-blueviolet",
         "https://github.com/SciML/ColPrac"
     )
 end
