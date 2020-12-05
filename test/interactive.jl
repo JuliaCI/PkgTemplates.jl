37b0a3252ea32bf41b641ea3b71ad573aea253c8
Roger-luo <rogerluo.rl18@gmail.com>
2020-12-05 16:30:55 -0500

    fix tests

--- test/interactive.jl
+++ test/interactive.jl
@@ -125,10 +125,10 @@ end
         @testset "Disabling default plugins" begin
             print(
                 stdin.buffer,
-                CR, DOWN^5, CR, DONE,  # Customize user and plugins
+                CR^2, DOWN^5, CR, DONE,  # Customize user and plugins
                 USER, LF,                # Enter user
                 SELECT_DEFAULTS,         # Pre-select default plugins
-                UP, CR, UP^2, CR, DONE,  # Disable TagBot and Readme
+                UP^2, CR, UP^2, CR, DONE,  # Disable TagBot and Readme
                 DONE^(NDEFAULTS - 2),    # Don't customize plugins
             )
             @test Template(; interactive=true) == Template(;
