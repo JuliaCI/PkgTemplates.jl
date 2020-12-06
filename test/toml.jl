4a5d11249e6cd4832bc48225f6143041ef6ba1a8
Roger-luo <rogerluo.rl18@gmail.com>
2020-12-05 21:24:32 -0500

    fix toml test

--- test/toml.jl
+++ test/toml.jl
@@ -1,10 +1,10 @@
 @info "testing TOML conversion"
 
-t = Template(;user="me", dir="~/.julia/dev")
+t = Template(;user="me", authors = ["Tester <te@st.er> and contributors"], dir="~/.julia/dev")
 @test from_dict(Template, to_dict(t)) == t
 @test toml(t) == """
 user = "me"
-authors = ["Roger-luo <rogerluo.rl18@gmail.com> and contributors"]
+authors = ["Tester <te@st.er> and contributors"]
 dir = "~/.julia/dev"
 host = "github.com"
 julia = "1.0.0"
@@ -12,7 +12,7 @@ julia = "1.0.0"
 
 src = """
 user = "me"
-authors = ["Roger-luo <rogerluo.rl18@gmail.com> and contributors"]
+authors = ["Tester <te@st.er> and contributors"]
 dir = "~/.julia/dev"
 host = "github.com"
 julia = "1.0.0"
@@ -27,7 +27,10 @@ julia = "1.0.0"
 """
 d = TOML.parse(src)
 
-@test from_dict(Template, d) == Template(;user="me", dir="~/.julia/dev", plugins=[!Git])
+@test from_dict(Template, d) == Template(;user="me",
+    authors=["Tester <te@st.er> and contributors"],
+    dir="~/.julia/dev", plugins=[!Git]
+)
 
 # https://github.com/JuliaLang/TOML.jl/issues/13
 @test_broken toml(t) == src
