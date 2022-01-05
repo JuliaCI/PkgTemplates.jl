3e6ee73ed590ff661164a713b5d48b1d887d37d6
Miles Lucas <mdlucas@hawaii.edu>
2022-01-05 17:00:02 -0600

    export PkgEvalBadge

--- src/plugins/badges.jl
+++ src/plugins/badges.jl
@@ -44,8 +44,8 @@ struct PkgEvalBadge <: BadgePlugin end
 
 function badges(::PkgEvalBadge)
     return Badge(
-    "PkgEval",
-    "https://juliaci.github.io/NanosoldierReports/pkgeval_badges/{{{PKG1}}}/{{{PKG}}}.svg",
-    "https://juliaCI.github.io/NanosoldierReports/pkgeval_badges/report.html"
+        "PkgEval",
+        "https://juliaci.github.io/NanosoldierReports/pkgeval_badges/{{{PKG1}}}/{{{PKG}}}.svg",
+        "https://juliaCI.github.io/NanosoldierReports/pkgeval_badges/report.html"
     )
 end
