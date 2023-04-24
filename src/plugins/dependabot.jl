b88b1aa821c65c7eb8d0b17fa2c6c7a99584a1ba
Hendrik Ranocha <mail@ranocha.de>
2023-04-24 07:59:27 +0200

    explicit note about package ecosystems

--- src/plugins/dependabot.jl
+++ src/plugins/dependabot.jl
@@ -5,6 +5,13 @@ Setups Dependabot to create PRs whenever GitHub actions can be updated.
 This is very similar to [`CompatHelper`](@ref), which performs the same task
 for Julia package dependencies.
 
+!!! note "Only for GitHub actions"
+    Currently, this plugin is configured to setup Dependabot only for the
+    GitHub actions package ecosystem. For example, it will create PRs whenever
+    GitHub actions such as `uses: actions/checkout@v2` can be updated to
+    `uses: actions/checkout@v3`. If you want to configure Dependabot to update
+    other package ecosystems, please modify the resulting file yourself.
+
 ## Keyword Arguments
 - `file::AbstractString`: Template file for `dependabot.yml`.
 """
