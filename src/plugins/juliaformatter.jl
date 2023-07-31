15cca5829ab732ed4b21fc8931c2cff038bd6f31
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-07-31 12:40:47 +0200

    Add default option for empty style

--- src/plugins/juliaformatter.jl
+++ src/plugins/juliaformatter.jl
@@ -1,26 +1,33 @@
 """
     JuliaFormatter(;
         file="$(contractuser(default_file(".JuliaFormatter.toml")))",
-        style="sciml"
+        style=""
     )
 
-Creates a `.JuliaFormatter.toml` file where the formatting style is specified.
+Create a `.JuliaFormatter.toml` file, used by [JuliaFormatter.jl](https://github.com/domluna/JuliaFormatter.jl) and the Julia VSCode extension to configure automatic code formatting.
 
-This file is used by [JuliaFormatter.jl](https://github.com/domluna/JuliaFormatter.jl) and the Julia VSCode extension to configure automatic formatting.
-It can be further customized by the user beyond the style name, see the [JuliaFormatter.jl docs](https://domluna.github.io/JuliaFormatter.jl/stable/).
+This file can be entirely customized by the user, see the [JuliaFormatter.jl docs](https://domluna.github.io/JuliaFormatter.jl/stable/).
 
 ## Keyword Arguments
 - `file::String`: Template file for `.JuliaFormatter.toml`.
-- `style::String`: Compatible style name (either "sciml", "blue" or "yas")
+- `style::Union{Nothing,String}`: Style name, defaults to the empty string `""` for no style but can also be one of `("sciml", "blue", "yas")` for a preconfigured style.
 """
 @plugin struct JuliaFormatter <: FilePlugin
     file::String = default_file(".JuliaFormatter.toml")
-    style::String = "sciml"
+    style::String = ""
 end
 
 source(p::JuliaFormatter) = p.file
 destination(::JuliaFormatter) = ".JuliaFormatter.toml"
 
-view(p::JuliaFormatter, t::Template, pkg::AbstractString) = Dict(
-    "STYLE" => p.style,
-)
+function view(p::JuliaFormatter, t::Template, pkg::AbstractString)
+    d = Dict{String,String}()
+    if p.style == ""
+        d["STYLE"] = ""
+    elseif p.style in ("blue", "sciml", "yas")
+        d["STYLE"] = """style = \"$(p.style)\""""
+    else
+        throw(ArgumentError("Formatting style not recognized"))
+    end
+    return d
+end
