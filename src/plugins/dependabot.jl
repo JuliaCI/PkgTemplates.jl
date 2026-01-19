42225fabe86c508290f0a61be73e7d82548bf661
Dilum Aluthge <dilum@aluthge.com>
2026-01-19 10:36:47 -0500

    Update the docstring for `Dependabot` to reflect Julia package support

--- src/plugins/dependabot.jl
+++ src/plugins/dependabot.jl
@@ -1,14 +1,13 @@
 """
     Dependabot(; file="$(contractuser(default_file("github", "dependabot.yml")))")
 
-Setups Dependabot to create PRs whenever GitHub actions can be updated.
-This is very similar to [`CompatHelper`](@ref), which performs the same task
-for Julia package dependencies.
+Setups Dependabot to create PRs whenever Julia packages (`[compat]` entries) or GitHub Actions can be updated.
 
-!!! note "Only for GitHub actions"
+!!! note "Only for Julia packages and GitHub Actions"
     Currently, this plugin is configured to setup Dependabot only for the
-    GitHub actions package ecosystem. For example, it will create PRs whenever
-    GitHub actions such as `uses: actions/checkout@v5` can be updated to
+    Julia and GitHub Actions package ecosystems. For example, it will create PRs
+    whenever a Julia `[compat]` entry `Foo = "1"` can be updated to `Foo = "1, 2"`,
+    and whenever GitHub actions such as `uses: actions/checkout@v5` can be updated to
     `uses: actions/checkout@v6`. If you want to configure Dependabot to update
     other package ecosystems, please modify the resulting file yourself.
 
